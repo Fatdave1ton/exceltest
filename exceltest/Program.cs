e95805a2582f89ec009d3ebef12391bab4e3e517
Dave Worthington <dave@dave.com>
2019-08-27 23:29:10 +0100

    init

--- exceltest/Program.cs
+++ exceltest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
 
 namespace exceltest
 {
@@ -10,6 +11,34 @@ namespace exceltest
 	{
 		static void Main(string[] args)
 		{
+
+			int n = 0;
+			do
+			{
+				openExcel();
+				Console.WriteLine(n);
+				n++;
+				//System.Threading.Thread.Sleep(1000);
+				
+			} while (n < 20);
+			Console.ReadKey();			
+		}
+		static void openExcel()
+		{
+			Application excelInstance = new Application();
+			Workbook test;
+			Workbooks
+				tmp;
+
+			tmp = excelInstance.Workbooks;
+
+			test = tmp.Open("f:\\helloworld.xlsm");
+			excelInstance.Visible = true;
+			excelInstance.Run("Sheet1.test");
+			excelInstance.Quit();
+			System.Runtime.InteropServices.Marshal.ReleaseComObject(test);
+			System.Runtime.InteropServices.Marshal.ReleaseComObject(tmp);
+			System.Runtime.InteropServices.Marshal.ReleaseComObject(excelInstance);
 		}
 	}
 }
